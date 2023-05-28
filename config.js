d9e45e2e898499dcfef4c6a3f56b66ae54af22cf
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2023-05-29 01:22:51 +0500

    first commit

---
+++ config.js
@@ -0,0 +1,29 @@
+var config = {
+    // The length of the sentence can be arbitrary, you can write ten sentences, twenty sentences are fine
+    // Try not to exceed 15 characters per sentence, otherwise the display effect may not be very good
+    // Here, the last comma at the end of each sentence must be in English!! Very important
+    texts: [
+        "Hey, Senorita",
+        "Wishing you the Happiest Birthday",
+        "May this be the brightest day",
+        "Happy 28th Birthday🎉🎉🎉🎉🎉",
+        "Make a wish :D",
+        "Yayyyyyyyyyyyyyyyyyyyyy",
+        "Sunooooo...",
+        "Jiyo, Hanso, Muskarao",
+        "Kya Pata",
+        "Kal Ho Na Ho"
+    ],
+    /**
+      * imgs can be left blank, but if you want to fill in, you must follow the format below
+      * "Corresponding to the above text, it must be exactly the same" : "The image address, you can put the image in the imgs folder"
+      * E.g
+      * "Beloved little cutie": "./imgs/xiaokeai.jpg"
+      *
+      * If you don't want pictures, just write two slash comments at the beginning of each line, for example, the picture "Today is your birthday" below will not be displayed :)
+      * Tip: It is better to use a square or close to a square for the picture, it looks better
+      */
+    imgs: {
+        "item": "./imgs/img.jpg",
+    },
+};
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Hey Ice Cream:)
+## [Click it](https://hamzazafarbhatti.github.io/birthaday-tayyaba/)
